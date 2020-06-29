643f4ed89b93b2a4962473189f1ca8f2dd170494
kenan <kenan@LAPTOP-5KTTCAE1>
2020-06-29 09:21:38 +0200

    Add project files.

---
+++ Scaffolding/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scaffolding.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        [MaxLength(50)]
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ Scaffolding/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scaffolding.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
